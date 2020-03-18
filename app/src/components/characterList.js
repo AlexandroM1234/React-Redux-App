058387201eff2ab096d482cae39672451fb21ff3
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-18 18:59:38 -0400

    ITS WORKING

--- app/src/components/characterList.js
+++ app/src/components/characterList.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getCharacter } from "../actions/actions";
 
 const CharacterList = props => {
-  console.log(props);
-  return <div></div>;
+  console.log(props.characters);
+  return (
+    <div>
+      {props.characters.map(character => (
+        <p>{character.name}</p>
+      ))}
+    </div>
+  );
 };
 
 const mapStatetoProps = state => {
-  console.log("Tis state", state);
-
   return {
-    // characters: state.characters,
-    // isFetchingData: state.isFetchingData
+    characters: state.characters,
+    isFetchingData: state.isFetchingData
   };
 };
 
-export default connect(mapStatetoProps, { getCharacter })(CharacterList);
+export default connect(mapStatetoProps, {})(CharacterList);
