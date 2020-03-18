7dcbcdd13930087f3871b6ab379006081247c6db
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-18 19:03:10 -0400

    it styled

--- app/src/components/characterList.js
+++ app/src/components/characterList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  border: red 2px solid;
+  border-radius: 15px;
+  margin: 8%;
+  padding: 2%;
+`;
 
 const CharacterList = props => {
   console.log(props.characters);
   return (
     <div>
       {props.characters.map(character => (
-        <p>{character.name}</p>
+        <StyledDiv>{character.name}</StyledDiv>
       ))}
     </div>
   );
